058df57e4e4af6adaf4b04ee9b5d4f63e463970f
Kevin-CrA <a1117140138@utch.edu.mx>
2022-03-17 18:22:17 -0700

    fixing imports

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request
 from flask_restful import Api, Resource, reqparse, abort
 from flask_pymongo import pymongo
 from flask_cors import CORS
-import db_config as database
+import app.db_config as database
 
 app =Flask(__name__)
 api = Api(app)

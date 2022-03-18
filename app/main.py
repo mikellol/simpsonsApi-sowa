c9d669633e580a95eed7295bfc32e5be64428e62
Kevin-CrA <a1117140138@utch.edu.mx>
2022-03-17 17:50:40 -0700

    CORS config

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 from flask import Flask, jsonify, request
 from flask_restful import Api, Resource, reqparse, abort
 from flask_pymongo import pymongo
+from flask_cors import CORS
 import db_config as database
 
 app =Flask(__name__)
 api = Api(app)
+CORS(app)
 
 post_students_args= reqparse.RequestParser()
 

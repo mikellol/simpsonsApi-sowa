eece7dfee73f5c1b089ddb2d832eb3d870629211
Kevin-CrA <a1117140138@utch.edu.mx>
2022-03-29 22:58:00 -0700

    git first commit

--- app/main.py
+++ app/main.py
@@ -8,117 +8,121 @@ app =Flask(__name__)
 api = Api(app)
 CORS(app)
 
-post_students_args= reqparse.RequestParser()
-post_students_args.add_argument("id",type=int,help="ERROR id value needs to be an integer", required=True)
-post_students_args.add_argument("first_name",type=str,help="ERROR first_name is required", required=True)
-post_students_args.add_argument("last_name",type=str,help="ERROR last_name is required", required=True)
-post_students_args.add_argument("image",type=str,help="ERROR you need to add the image url", required=True)
-post_students_args.add_argument("group",type=str, required=False)
-post_students_args.add_argument("career",type=str, required=False)
+'''  '''
+post_characters_args= reqparse.RequestParser()
+post_characters_args.add_argument("id",type=int,help="ERROR id value needs to be an integer", required=True)
+post_characters_args.add_argument("name",type=str,help="ERROR name is required", required=True)
+post_characters_args.add_argument("gender",type=str,help="ERROR gender is required", required=True)
+post_characters_args.add_argument("image",type=str,help="ERROR you need to add the image url", required=True)
 
 
-patch_students_args=reqparse.RequestParser()
-patch_students_args.add_argument("id",type=int,help="ERROR id value needs to be an integer", required=False)
-patch_students_args.add_argument("first_name",type=str,help="ERROR first_name is required", required=False)
-patch_students_args.add_argument("last_name",type=str,help="ERROR last_name is required", required=False)
-patch_students_args.add_argument("image",type=str,help="ERROR you need to add the image url", required=False)
-patch_students_args.add_argument("group",type=str, required=False)
-patch_students_args.add_argument("career",type=str, required=False)
+patch_characters_args=reqparse.RequestParser()
+patch_characters_args.add_argument("id",type=int,help="ERROR id value needs to be an integer", required=False)
+patch_characters_args.add_argument("name",type=str,help="ERROR name is required", required=False)
+patch_characters_args.add_argument("gender",type=str,help="ERROR gender is required", required=False)
+patch_characters_args.add_argument("image",type=str,help="ERROR you need to add the image url", required=False)
+
+
 
 class Test(Resource):
     def get(self):
         return jsonify({"message":"You are connected"})
 
-
-class Students(Resource):
+''' Class for getting the list of characters using a for to call every character, retunring it as a json response '''
+class Characters(Resource):
     def get(self):
-        response= list(database.db.students.find())
-        students=[]
-        for student in response:
-            del student['_id']
-            students.append(student)
-        return jsonify({'results':students})
+        response= list(database.db.characters.find())
+        characters=[]
+        for character in response:
+            del character['_id']
+            characters.append(character)
+        return jsonify({'results':characters})
 
 
-class Student(Resource):
+''' Class for manipulating data from a single character '''
+class Character(Resource):
 
+    ''' Returns the information from a character using the variable 'id' from the list of characters '''
     def get(self,id):
-        response = database.db.students.find_one({'id':id})
+        response = database.db.characters.find_one({'id':id})
         del response['_id']
         return jsonify(response)
-    
+
+    ''' The post method let us create a new character using a set of requirements to fill, in case of a duplicate of an id an error will be shown'''
     def post(self):
-        args=post_students_args.parse_args()
-        self.abort_if_id_exist(args['id'])      #self.abort_if_id_exist(request.json['id'])
+        args=post_characters_args.parse_args()
+        self.abort_if_id_exist(args['id'])    
         #print(args)
-        database.db.students.insert_one({
+        database.db.characters.insert_one({
             'id': args['id'],
-            'first_name': args['first_name'],
-            'last_name': args['last_name'],
+            'name': args['name'],
+            'gender': args['gender'],
             'image': args['image'],
-            'group': args['group'],
-            'career': args['career'],
-            #'career': request.json['career'],
+
+           
         })
         return jsonify(args)
-        #return jsonify({"200": request.json})
+        
 
+    ''' The put method let us modify the information from a character only when we refill every slot of information and in case of a non existing is, it will show us an error '''
     def put(self,id):
-        args=post_students_args.parse_args()
+        args=post_characters_args.parse_args()
         self.abort_if_not_exist(id)
-        database.db.students.update_one(
+        database.db.characters.update_one(
             {'id':id},
             {'$set':{
                 'id': args['id'],
-                'first_name': args['first_name'],
-                'last_name': args['last_name'],
+                'name': args['name'],
+                'gender': args['gender'],
                 'image': args['image'],
-                'group': args['group'],
-                'career': args['career'],
+
             }}
         )
         return jsonify(args)
 
+    ''' The patch method let us replace modify infromation from a character, and being able to only modify what we need without having to fill every infromation slot again '''
     def patch(self,id):
-        student= self.abort_if_not_exist(id)
-        args= patch_students_args.parse_args()
+        character= self.abort_if_not_exist(id) #In case of a non exsiting id, it will show us an error
+        args= patch_characters_args.parse_args()
         
-        database.db.students.update_one(
+        database.db.characters.update_one(
             {'id':id},
             {'$set':{
-                'id': args['id'] or student['id'],
-                'first_name': args['first_name'] or student['first_name'],
-                'last_name': args['last_name'] or student['last_name'],
-                'image': args['image'] or student['image'],
-                'group': args['group'] or student['group'],
-                'career': args['career'] or student['career'],
+                'id': args['id'] or character['id'],
+                'name': args['name'] or character['name'],
+                'gender': args['gender'] or character['gender'],
+                'image': args['image'] or character['image'],
+      
             }}
         )
-        student= self.abort_if_not_exist(id)
-        del student['_id']
-        return jsonify(student)
+        character= self.abort_if_not_exist(id)
+        del character['_id']
+        return jsonify(character)
     
+    ''' The delete method let us delete all the information in the database from an character using the variable "id" '''
     def delete(self,id):
-        student=self.abort_if_not_exist(id)
-        database.db.students.delete_one({'id':id})
-        del student['_id']
-        return jsonify({'deleted':student})
+        character=self.abort_if_not_exist(id) #In case of a non exsiting id, it will show us an error
+        database.db.characters.delete_one({'id':id})
+        del character['_id']
+        return jsonify({'deleted':character})
 
+    ''' Method to show an error, stopping the process, when we are trying to create a new character with an existing id '''
     def abort_if_id_exist(self,id):
-        if database.db.students.find_one({'id':id}):
-            abort(jsonify({'error':{'406':f"The student with the id: {id} already exist"}}))
+        if database.db.characters.find_one({'id':id}):
+            abort(jsonify({'error':{'406':f"The character with the id: {id} already exist"}}))
 
+    ''' Method to show an error, stopping the process, when we are trying to modify information from a character with a non existing id '''
     def abort_if_not_exist(self,id):
-        student=database.db.students.find_one({'id':id})
-        if not student: 
-            abort(jsonify({'error':{'404':f"The student with the id: {id} not found"}}))
+        character=database.db.characters.find_one({'id':id})
+        if not character: 
+            abort(jsonify({'error':{'404':f"The character with the id: {id} not found"}}))
         else:
-            return student
+            return character
 
 
 api.add_resource(Test,'/test/')
-api.add_resource(Students,'/students/')
-api.add_resource(Student,'/student/', '/student/<int:id>/')
+api.add_resource(Characters,'/characters/')
+api.add_resource(Character,'/character/', '/character/<int:id>/')
 
 if __name__=='__main__':
     app.run(load_dotenv=True, port=8080)
